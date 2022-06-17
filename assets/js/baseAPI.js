8bb0a5b9f330935b0b203600191ba8e2d8320fb9
licheng <374596661@qq.com>
2022-06-18 03:06:19 +0800

    获取用户信息，完成登录退出

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,5 +3,10 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter( function( options ) {
     options.url = 'http://www.liulongbin.top:3007'+options.url
-   console.log(options.url);
+      // 统一为有权限的接口，设置 headers 请求头
+    if(options.url.indexOf('/my/')!==-1){
+      options.headers = {
+        Authorization: localStorage.getItem('token') || ''
+      }
+    }
   });
